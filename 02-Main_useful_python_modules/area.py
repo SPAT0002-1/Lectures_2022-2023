4efe554f8213c297f9718c33eb91ad35d6b0708f
dsluse <slusedominique@yahoo.fr>
2020-02-17 21:34:32 +0100

    Add filled notebooks from lecture 2

---
+++ 02-Main_useful_python_modules/area.py
@@ -0,0 +1,12 @@
+def square(x):
+    '''
+    Gives the area of a square
+    '''
+    return x**2
+
+def circle(r):
+    '''
+    Gives the area of a circle
+    '''
+    pi = 3.14116
+    return pi * r**2
